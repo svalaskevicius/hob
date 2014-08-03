16f0297cf4808b85dd1affcc8d42395a110b1b12
Sarunas Valaskevicius <rakatan@gmail.com>
2014-08-03 15:53:07 +0100

    open new tabs

--- src/hob.hs
+++ src/hob.hs
@@ -10,37 +10,68 @@ import Graphics.UI.Gtk.ModelView as Mv
 import Graphics.UI.Gtk.General.StyleContext
 import System.Directory
 import System.FilePath
+import System.IO (hPutStrLn, stderr)
+import Data.Maybe (fromJust)
 
--- import Graphics.UI.Gtk.ModelView.TreeStore
 
-fileTreeFromDirectory :: FilePath -> IO (Forest String)
+data DirectoryTreeElement = DirectoryTreeElement String FilePath
+
+directoryTreeElementLabel :: DirectoryTreeElement -> String
+directoryTreeElementLabel (DirectoryTreeElement label _) = label
+
+directoryTreeElementPath :: DirectoryTreeElement -> FilePath
+directoryTreeElementPath (DirectoryTreeElement _ path) = path
+
+type NewFileEditorLauncher = FilePath -> IO()
+
+
+
+main = do
+    initGUI
+
+    builder <- builderNew
+    builderAddFromFile builder "ui/ui.glade"
+
+    sidebarTree <- builderGetObject builder castToTreeView "directoryListing"
+    mainEditNotebook <- builderGetObject builder castToNotebook "tabbedEditArea"
+    initSideBarFileTree sidebarTree $ launchNewFileEditor mainEditNotebook
+
+    setGtkStyle
+
+    mainWindow <- builderGetObject builder castToWindow "mainWindow"
+    mainWindow `on` deleteEvent $ liftIO mainQuit >> return False
+    widgetShowAll mainWindow
+
+    mainGUI
+
+
+fileTreeFromDirectory :: FilePath -> IO (Forest DirectoryTreeElement)
 fileTreeFromDirectory path = do
     contents <- getDirectoryContents path
     forM (removeDotDirectories contents) $ \ child -> do
         let childPath = path </> child
         isDir <- doesDirectoryExist childPath
         childrenForest <- if isDir then fileTreeFromDirectory childPath else return []
-        return $ Node child childrenForest
+        return $ Node (DirectoryTreeElement child childPath) childrenForest
     where
         removeDotDirectories = filter (\child -> not ((child == ".") || (child == "..")))
 
 setGtkStyle = do
-    --ctx <- widgetGetStyleContext mainWindow
     cssProvider <- cssProviderNew
     cssProviderLoadFromPath cssProvider ("ui" </> "themes" </> "adwaitaGrnPlus" </> "gtk-dark.css")
-    --styleContextAddProvider ctx cssProvider 800
     maybe (return()) (\screen -> styleContextAddProviderForScreen screen cssProvider 800) =<< screenGetDefault
 
 
-initSideBarFileTree treeView = do
+initSideBarFileTree :: TreeView -> NewFileEditorLauncher -> IO ()
+initSideBarFileTree treeView launchFile = do
     treeModel <- treeStoreNew =<< fileTreeFromDirectory =<< getCurrentDirectory
-    customStoreSetColumn treeModel (makeColumnIdString 0) id
+    customStoreSetColumn treeModel (makeColumnIdString 0) directoryTreeElementLabel
 
     col <- treeViewColumnNew
 
     rend <- Mv.cellRendererTextNew
     Mv.cellLayoutPackStart col rend True
-    Mv.cellLayoutSetAttributes col rend treeModel (\v -> [Mv.cellText := v])
+    Mv.cellLayoutSetAttributes col rend treeModel (\v -> [Mv.cellText := directoryTreeElementLabel v])
 
     treeViewAppendColumn treeView col
 
@@ -50,19 +81,16 @@ initSideBarFileTree treeView = do
     treeViewSetSearchColumn treeView $ makeColumnIdString 0
 
     treeView `on` rowCollapsed $ \ _ _ -> treeViewColumnsAutosize treeView
+    treeView `on` rowActivated $ \ path _ -> (launchFile . directoryTreeElementPath) =<< treeStoreGetValue treeModel path
+
+    return ()
 
 
-main = do
-    initGUI
+launchNewFileEditor :: Notebook -> NewFileEditorLauncher
+launchNewFileEditor targetNotebook filepath = do
+    editor <- textViewNew
+    widgetShowAll editor
+    notebookAppendPage targetNotebook editor "t"
+    notebookSetShowTabs targetNotebook True
+    return()
 
-    builder <- builderNew
-    builderAddFromFile builder "ui/ui.glade"
-
-    initSideBarFileTree =<< builderGetObject builder castToTreeView "directoryListing"
-
-    setGtkStyle
-
-    mainWindow <- builderGetObject builder castToWindow "mainWindow"
-    mainWindow `on` deleteEvent $ liftIO mainQuit >> return False
-    widgetShowAll mainWindow
-    mainGUI
