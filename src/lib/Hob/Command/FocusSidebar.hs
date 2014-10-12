9dfc666baed5b02839875abe5a2a7b84a0db704f
Sarunas Valaskevicius <rakatan@gmail.com>
2014-10-12 23:51:42 +0100

    search strictly by filepath when syncing

--- src/lib/Hob/Command/FocusSidebar.hs
+++ src/lib/Hob/Command/FocusSidebar.hs
@@ -10,7 +10,7 @@ import Hob.Context
 import Hob.Context.UiContext
 import Hob.Control
 import Hob.Ui.Editor         (getActiveEditor, getEditorFilePath)
-import Hob.Ui.SidebarSearch  (startSidebarSearch)
+import Hob.Ui.Sidebar        (pathColumn, activateSidebarPath)
 
 focusSidebarCommandHandler :: CommandHandler
 focusSidebarCommandHandler = CommandHandler Nothing focusSidebar
@@ -24,7 +24,19 @@ focusSidebar = widgetGrabFocus . sidebarTree . uiContext
 syncActiveEditorPathToSidebar :: Context -> IO ()
 syncActiveEditorPathToSidebar ctx = maybeDo syncToEditor =<< getActiveEditor ctx
     where syncToEditor editor = maybeDo syncToFilePath =<< getEditorFilePath editor
-          syncToFilePath = startSidebarSearch ctx
+          syncToFilePath filePath = do
+              let treeView = sidebarTree.uiContext $ ctx
+              model <- treeViewGetModel treeView
+              maybeDo (syncTreeViewModel filePath treeView) model
+          syncTreeViewModel filePath treeView model = 
+              treeModelForeach model (\iter -> do
+                      path <- treeModelGetValue model iter pathColumn
+                      if (path == filePath) then do
+                          modelPath <- treeModelGetPath model iter
+                          activateSidebarPath treeView modelPath
+                          return True 
+                      else return False
+                  )
 
 syncFocusSidebar :: Context -> IO ()
 syncFocusSidebar ctx = do
